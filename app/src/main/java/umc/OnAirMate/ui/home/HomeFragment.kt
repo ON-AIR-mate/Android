b93415274d00fd837085b3f9fb078ba33662d1b2
iwannacat <ju0808lie@naver.com>
2025-07-15 13:16:24 +0900

    feat: 방 검색 드롭다운 디자인 커스텀

--- app/src/main/java/umc/OnAirMate/ui/home/HomeFragment.kt
+++ app/src/main/java/umc/OnAirMate/ui/home/HomeFragment.kt
@@ -1,17 +1,18 @@
 package umc.onairmate.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
+import android.widget.AdapterView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import umc.onairmate.R
 import umc.onairmate.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    private val TAG = javaClass.simpleName
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
@@ -30,13 +31,8 @@ class HomeFragment : Fragment() {
         binding.rvRooms.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL,false)
         binding.rvRooms.adapter = adapter
 
-//        binding.rvContinuingRooms.layoutManager = LinearLayoutManager(requireContext(),
-//            LinearLayoutManager.VERTICAL, false)
-//        binding.rvContinuingRooms.adapter = adapter
+        setSearchSpinner()
 
-        val searchTypeList = listOf("영상 제목", "방 제목", "방장 닉네임")
-        val searchAdapter = SearchTypeSpinnerAdapter(requireContext(),searchTypeList)
-        binding.spinnerSearchType.adapter = searchAdapter
 
         return binding.root
     }
@@ -44,5 +40,24 @@ class HomeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setSearchSpinner(){
+        val searchTypeList = listOf("영상 제목", "방 제목", "방장 닉네임")
+        val searchAdapter = SearchTypeSpinnerAdapter(requireContext(),searchTypeList)
+        binding.spinnerSearchType.adapter = searchAdapter
+        binding.spinnerSearchType.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedItem = parent?.getItemAtPosition(position) as String
+                Log.d(TAG,"selected Item ${selectedItem}")
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
     }
 }
