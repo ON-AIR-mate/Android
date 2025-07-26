c0486257738af778740db37fefdeb7d8eb675b5b
iwannacat <ju0808lie@naver.com>
2025-07-26 13:11:28 +0900

    fix: fragment에 의존성 주입

--- app/src/main/java/umc/OnAirMate/ui/friend/FriendFragment.kt
+++ app/src/main/java/umc/OnAirMate/ui/friend/FriendFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
 import umc.OnAirMate.databinding.FragmentFriendBinding
 
+@AndroidEntryPoint
 class FriendFragment : Fragment() {
 
     private var _binding: FragmentFriendBinding? = null
