6b087aedb2ec6a18908456392d8156e2d1a654ba
Jeongmin Park <60566546+jeongmin-0113@users.noreply.github.com>
2025-07-26 15:53:27 +0900

    feat: drawer 작업

---
+++ app/src/main/java/umc/OnAirMate/ui/chat_room/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package umc.OnAirMate.ui.chat_room.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
