d8835d271d13ffeef7d1d06777193a04c2221a15
Jeongmin Park <60566546+jeongmin-0113@users.noreply.github.com>
2025-07-31 22:46:23 +0900

    feat: 썸네일, 프로필 로드 함수 분리

--- app/src/main/java/umc/onairmate/ui/util/NetworkImageLoader.kt
+++ app/src/main/java/umc/onairmate/ui/util/NetworkImageLoader.kt
@@ -7,15 +7,26 @@ import umc.onairmate.R
 /**
  * 네트워크 이미지 로더 (썸네일 유틸)
  *
- * 사용법: NetworkImageLoader.load(이미지뷰, url)
+ * 사용법: NetworkImageLoader.~~Load(이미지뷰, url)
  */
 object NetworkImageLoader {
-    fun load(imageView: ImageView, url: String) {
+    // 사용법: NetworkImageLoader.thumbnailLoad(이미지뷰, url)
+    fun thumbnailLoad(imageView: ImageView, url: String) {
         imageView.load(url) {
             // 로딩중 이미지
             placeholder(R.drawable.img_loading)
             // 로딩 실패시 이미지
             error(R.drawable.img_fail)
+        }
+    }
+
+    // 사용법: NetworkImageLoader.profileLoad(이미지뷰, url)
+    fun profileLoad(imageView: ImageView, url: String) {
+        imageView.load(url) {
+            // 로딩중 이미지
+            placeholder(R.drawable.ic_empty_profile)
+            // 로딩 실패시 이미지
+            error(R.drawable.ic_empty_profile)
         }
     }
 }
