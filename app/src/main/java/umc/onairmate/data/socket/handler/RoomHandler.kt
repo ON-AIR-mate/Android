1d4933e03979f33cd754fbf1622b779acb2e8ee2
Jeongmin Park <60566546+jeongmin-0113@users.noreply.github.com>
2025-08-15 16:13:08 +0900

    fix: 머지 오류 수정

--- app/src/main/java/umc/onairmate/data/socket/handler/RoomHandler.kt
+++ app/src/main/java/umc/onairmate/data/socket/handler/RoomHandler.kt
@@ -2,7 +2,7 @@ package umc.onairmate.data.socket.handler
 
 import android.util.Log
 import org.json.JSONObject
-import umc.onairmate.data.model.entity.SocketError
+import umc.onairmate.data.model.entity.SocketMessage
 import umc.onairmate.data.model.entity.VideoPauseData
 import umc.onairmate.data.model.entity.VideoPlayData
 import umc.onairmate.data.model.entity.VideoSyncData
@@ -19,7 +19,7 @@ class RoomHandler(
                 Log.d("RoomHandler","sync ${data}")
                 val parsed = parseJson<VideoSyncData>(data)
                 if (parsed == null) {
-                    val error = SocketError(type = "receiveVideoSync", message = data.toString())
+                    val error = SocketMessage(type = "receiveVideoSync", message = data.toString())
                     listener.onError(error)
                 }
                 else listener.syncVideo(parsed)
@@ -28,7 +28,7 @@ class RoomHandler(
                 Log.d("RoomHandler","play ${data}")
                 val parsed = parseJson<VideoPlayData>(data)
                 if (parsed == null) {
-                    val error = SocketError(type = "receiveVideoPlay", message = data.toString())
+                    val error = SocketMessage(type = "receiveVideoPlay", message = data.toString())
                     listener.onError(error)
                 }
                 else listener.onVideoPlay(parsed)
@@ -37,19 +37,19 @@ class RoomHandler(
                 Log.d("RoomHandler","pause ${data}")
                 val parsed = parseJson<VideoPauseData>(data)
                 if (parsed == null) {
-                    val error = SocketError(type = "receiveVideoPause", message = data.toString())
+                    val error = SocketMessage(type = "receiveVideoPause", message = data.toString())
                     listener.onError(error)
                 }
                 else listener.onVideoPause(parsed)
             },
             "error" to { data ->
                 Log.d("RoomHandler","error ${data}")
-                val parsed = parseJson<SocketError>(data)
+                val parsed = parseJson<SocketMessage>(data)
                 if (parsed == null) {
                     Log.w("ChatRoomHandler", "SocketError 파싱 실패: $data")
                 }
                 // 파싱 실패 시 기본 에러 객체 생성
-                val safeError = parsed ?: SocketError(type = "JSON_PARSE_ERROR", message = data.toString())
+                val safeError = parsed ?: SocketMessage(type = "JSON_PARSE_ERROR", message = data.toString())
                 listener.onError(safeError)
             }
         )
--- app/src/main/java/umc/onairmate/data/socket/listener/VideoEventListener.kt
+++ app/src/main/java/umc/onairmate/data/socket/listener/VideoEventListener.kt
@@ -1,12 +1,12 @@
 package umc.onairmate.data.socket.listener
 
-import umc.onairmate.data.model.entity.SocketError
+import umc.onairmate.data.model.entity.SocketMessage
 import umc.onairmate.data.model.entity.VideoPauseData
 import umc.onairmate.data.model.entity.VideoPlayData
 import umc.onairmate.data.model.entity.VideoSyncData
 
 interface VideoEventListener {
-    fun onError(errorMessage: SocketError){}
+    fun onError(errorMessage: SocketMessage){}
     fun onVideoPlay(playData: VideoPlayData) {}
     fun onVideoPause(pauseData: VideoPauseData) {}
     fun syncVideo(syncData: VideoSyncData) {}
